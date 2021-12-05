6451219048145423032cf5aa1563873151f16fd1
magsmurphy <mmurphy@crokepark.ie>
2021-12-05 09:15:39 +0000

    My changes

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pandas import read_csv
+
+covid_credit = read_csv('Covid Credit.csv')
+
+print(covid_credit.head())
+
+
+
+
